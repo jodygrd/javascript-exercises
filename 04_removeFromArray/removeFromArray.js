5a1ef727625900c2f9c7ca2d5b20cc1c52003ea1
jody lipkin <lipkinjody@gmail.com>
2022-11-21 20:28:31 -0800

    Complete remove from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    
+    //iterate over the old array
+    for (let i = 0; i < array.length; i++){
+        //check all the arguments
+        for (arg of args) {
+            if (array[i] == arg){
+              array.splice(i, 1);
+              i--
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
